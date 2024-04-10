12691d90a39bc5add13a5b87616dbb1f6a09bd72
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-04-10 19:39:30 +0530

    Issue Resolved

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const app = express();
 const { CORS_ORIGIN, CORS_METHODS } = process.env;
 const port = process.env.PORT || 5000;
 const corsOptions = { 
-    origin: 'http://localhost:3000', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000', 
+    methods: process.env.CORS_METHODS.split(',') || ['GET', 'POST', 'PUT', 'DELETE'],
     allowHeaders: ['Content-Type'] 
 };
 
